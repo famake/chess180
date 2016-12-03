45363df13aadfd984c421bb7324c524896a4b6f3
famake <mikaelaasen@hotmail.com>
2016-12-03 01:54:03 +0100

    configuring the chess180

--- src/chess180/Config.java
+++ src/chess180/Config.java
@@ -2,6 +2,6 @@ package chess180;
 
 public class Config {
 
-	public static String accessToken = "ENTER_API_KEY_HERE";
+	public static String accessToken = "cbf68965ed822757aa59b0ea61e1ff839d3e26bb";
 
 }
